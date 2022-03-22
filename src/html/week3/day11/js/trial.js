18e796bd8f3da3a848dd733d5d66980e0dd4755b
s-yux <sylomrouza24@gmail.com>
2022-03-22 17:59:30 +0800

    Commit for 21/3/2022 && 22/3/2022

---
+++ src/html/week3/day11/js/trial.js
@@ -0,0 +1,9 @@
+document.write("Hi, this is in trial file! <br>");
+document.write(2022);
+document.write("<br>");
+document.write("THe value of 4+4 is: " + (4+4));
+document.write("<br>");
+document.write("2"+"4<br>");
+document.write(2,4)
+
+
