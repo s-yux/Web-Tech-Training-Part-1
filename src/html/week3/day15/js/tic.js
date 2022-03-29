ec60361205427e97c266a5ec995d4fdaa3c7fa41
s-yux <sylomrouza24@gmail.com>
2022-03-29 14:09:49 +0800

    Minor Refactor TicTacToe Game

--- src/html/week3/day15/js/tic.js
+++ src/html/week3/day15/js/tic.js
@@ -240,7 +240,7 @@ function myfunc() {
 }
 
 // Function to reset game
-function myfunc_2() {
+function resetBoard() {
     location.reload();
     document.getElementById('b1').value = '';
     document.getElementById("b2").value = '';
